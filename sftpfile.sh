25dfbf03655d686f8a8684b3b3f2b585aa0c9639
divya <divya.b.s@ncodeit.com>
2018-05-15 00:41:58 +0530

    file transfer between unix and linux usinfsftp

---
+++ sftpfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sftp usename@hostname/ipaddress
+password: ****
+sftp> ls 
+sftp> lls 
+sftp> get *.* 
+sftp> put *.* 
+- Create directory in sftp: mkdir [For local: lmkdir]
+- Change directory in sftp: cd [For local: lcd]
+- get / mget : download files to local linux machine
+- put / mput : upload files to remote linux machie
+
