ef48fc4d3182f5b505cc04d3964a5b63782579bc
divya <divya.b.s@ncodeit.com>
2018-05-15 11:14:30 +0530

    SFTP

--- sftpfile.sh
+++ sftpfile.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-sftp usename@hostname/ipaddress
-password: ****
+sftp root@devops01.corp.ncodeit.com/192.168.11.160
+password: ncodeit
 sftp> ls 
 sftp> lls 
 sftp> get *.* 
 sftp> put *.* 
-- Create directory in sftp: mkdir [For local: lmkdir]
-- Change directory in sftp: cd [For local: lcd]
-- get / mget : download files to local linux machine
-- put / mput : upload files to remote linux machie
 
